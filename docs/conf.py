44c5e8a609cad78716ad7f355e172cd5ec3e9dec
Keith Smith <keith.scotts@gmail.com>
2018-12-09 00:45:06 -0600

    Documentation Update
    Updating docs for readthedocs, create_order private_key, and sphinx building.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ author = 'Keith Smith'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
